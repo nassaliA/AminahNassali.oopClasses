573a1e10f601d45fce13b27c1df9b106b71bc6ff
AMINAH NASSALI <nassali.aminah7@gmail.com>
2023-03-01 14:50:37 +0200

    Aminah Nassali.OOPclasses.js

---
+++ oopclasses.js
@@ -0,0 +1,39 @@
+//OOP CLASS
+
+// Class 1: cup
+class cup {
+    constructor(name, shape, color, weight, manufacturer) {
+      this.name = name;
+      this.shape = shape;
+      this.color = color;
+      this.weight = weight;
+      this.manufacturer = manufacturer;
+    }
+  }
+  
+  // Objects for cup class
+  let cup1 = new cup("mug", "round", "green", 180, "NICE");
+  let cup2 = new cup("chalice", "large", "red", 25, "tikPlastics");
+  let cup3 = new cup("Demitasse", "oval", "blue", 10, "Britania" );
+  let cup4 = new cup("travelMug", "squareOval", "white", 65, " Mukwano");
+  let cup5 = new cup("coktailGlass", "circle", "brown", 95, "MUG");
+console.log(cup1, cup2, cup3, cup4, cup5)
+
+// Class 1: cup
+class cup {
+  constructor(name, shape, color, weight, manufacturer) {
+    this.name = name;
+    this.shape = shape;
+    this.color = color;
+    this.weight = weight;
+    this.manufacturer = manufacturer;
+  }
+}
+
+// Objects for cup class
+let cup1 = new cup("mug", "round", "green", 180, "NICE");
+let cup2 = new cup("chalice", "large", "red", 25, "tikPlastics");
+let cup3 = new cup("Demitasse", "oval", "blue", 10, "Britania" );
+let cup4 = new cup("travelMug", "squareOval", "white", 65, " Mukwano");
+let cup5 = new cup("coktailGlass", "circle", "brown", 95, "MUG");
+console.log(cup1, cup2, cup3, cup4, cup5)
